47322aece1086dbc1d46c830a076acae5f96c545
izibrahim <imi_zahoor@hotmail.com>
2022-03-05 09:33:18 +0300

    ACI Infrastructure as code using Terraform

--- bridge_domain_subnets.tf
+++ bridge_domain_subnets.tf
@@ -2,14 +2,19 @@
 resource "aci_subnet" "web_server_subnets" {
   parent_dn   = aci_bridge_domain.web_server_bd.id
   description = "subnet"
-  ip          = local.bridge_domain_subnets[count.index]
-  annotation  = "tag_subnet"
-  name_alias  = "alias_subnet"
-  scope       = ["private", "shared"]
-  count       = local.bd_subnet
+  for_each    = toset(local.bridge_domain_subnets)
+  ip          = each.value
+  #ip          = local.bridge_domain_subnets[count.index]
+  annotation = "tag_subnet"
+  name_alias = "alias_subnet"
+  scope      = ["private", "shared"]
+#  count      = local.subnet_condition == true ? 1 : 0
 
 }
 
+
+/*
 output "bd_subnets" {
   value = aci_subnet.web_server_subnets[*].ip
 }
+*/
--- variables.tf
+++ variables.tf
@@ -7,9 +7,10 @@ locals {
   url                       = "https://10.10.20.14"                            # ACI URL
   aci_tenant                = "Prod_webserver_Tenant"                          # This will create Tenant
   bridge_domain             = "Prod_webserver_domain"                          # This will create Bridge Domain
+  bridge_domain_subnets      = ["10.0.3.28/27", "10.0.5.28/27", "10.0.6.28/27"] # add subnet to bridge domain use [] empty if you dont want to add the subnets inside bridge domain
   prod_vrf                  = "Prod_webserver_vrf"                             # This will create VRF or Context
   cdp_policy                = "Prod_cdp_policy"                                # cdp policy name
-  cdp_policy_condition      = false                                             # or false  if this is true it will create CDP policy
+  cdp_policy_condition      = true                                       # or false  if this is true it will create CDP policy
   lldp_interface_policy     = "Prod_lldp_policy"                               # lldp policy name
   lldp_policy_condition     = true                                             # or false  if this is true it will create lldp policy
   mcp_policy                = "Prod_mcp_polciy"                                # mcp policy name
@@ -18,11 +19,9 @@ locals {
   vlan_pool_alloc_mode      = "static"                                         # dynamic
   vlan_pool_range_from      = "vlan-10"                                        # vlan range
   vlan_pool_range_to        = "vlan-20"                                        # vlan range
-  vlan_pool_check           = false                                        # or false if this is true it will create vlan pool
-  bridge_domain_subnets     = ["10.0.3.28/27", "10.0.5.28/27", "10.0.6.28/27"] # Bridge domain ip adress
-  bd_subnet                 = 3
-  physical_domain           = "Prod_phy_domain"
-  physical_domain_condition = false # This will create Phycial domain
+  vlan_pool_check           = true                                          # or false if this is true it will create vlan pool
+  physical_domain           = "Prod_phy_domain"                                # Add create physical domain
+  physical_domain_condition = true                                            # if vlan_pool_check is true and physical_domain_condition then phyical domain will be created
 }
 
 /*
